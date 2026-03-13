6fd0ebec327e168892bc7378f5b9b006eab1798c
Asilbek-Khujanazarov <asilbekkhujanazarov@gmail.com>
2026-03-13 22:44:14 +0500

    feat: set comment heroku environment

--- Dto/ProductDto.cs
+++
@@ -1,9 +0,0 @@
-   public class ProductDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string? ImageUrl { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-    }
